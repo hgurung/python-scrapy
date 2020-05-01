959f78b2d0bd882cb5466f2867dad462fef64400
Harris Gurung <h.gurung1@gmail.com>
2020-05-01 16:46:24 +0545

    Checkin logs and checkout logs added

--- checkout.php
+++ checkout.php
@@ -8,7 +8,7 @@ foreach($userData as $user) {
     $password = $user['password'];
     // $url = $topic['question_url'];
     if (!empty($username) && !empty($password)) {
-        $command = "scrapy crawl checkout -s LOG_FILE=logs/checkout.log -a checktype=checkout -a username=".$username." -a password=".$password."";
+        $command = "scrapy crawl checkout -s LOG_FILE=checkout.log -a checktype=checkout -a username=".$username." -a password=".$password."";
         echo "<pre>";
         echo $command;
         echo "<pre>";
